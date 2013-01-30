710a7ff92f09c2c1fe68d9cb1d106eced32ca8d3
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-30 01:00:41 -0800

    Revert "Add TomDoc to core.rb."
    This reverts commit 22e8dd075265a013501fedb2db80636d2cdb4cad.

--- lib/tam_tam/core.rb
+++ lib/tam_tam/core.rb
@@ -1,20 +1,9 @@
 require "tam_tam/version"
 
-# Public: The primary interface for creating new LogSet objects. Also
-# responsible for keeping a global registry of adapters.
 module TamTam
   @adapters ||= {}
 
   class << self
-    # Public: Initialize a new LogSet. Uses the specified adapter, else Adium
-    # if it's registered, else the first registered adapter.
-    #
-    # options - The Hash options used to determine the adapter and its options.
-    #           (default: {}):
-    #         :adapter  - A Symbol specifying the adapter to use.
-    #         :path     - A String file path to the log files for this client.
-    #
-    # Returns a new LogSet.
     def new(options = {})
       adapter_key = options[:adapter]
       adapter_key = :adium if adapter_key.nil? && adapters.include?(:adium)
@@ -23,28 +12,14 @@ module TamTam
       LogSet.new(adapter)
     end
 
-    # Public: Register a chat adapter.
-    #
-    # key     - The Symbol to register the adapter under.
-    # adapter - A Class implementing the adapter interface.
-    #
-    # Returns the adapter.
     def register_adapter(key, adapter)
       @adapters[key] = adapter
     end
 
-    # Public: Remove a chat adapter from the registry.
-    #
-    # key - The Symbol key of the adapter to remove.
-    #
-    # Returns the removed adapter.
     def unregister_adapter(key)
       @adapters.delete(key)
     end
 
-    # Public: A list of registered adapters.
-    #
-    # Returns an Array of Symbol adapter keys.
     def adapters
       @adapters.keys
     end
