fac4fac4931bde2ef23793299183500412e9d48b
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-30 01:20:51 -0800

    Use #match_array instead of #eq for portability.

--- spec/tam_tam/adapters/adium_spec.rb
+++ spec/tam_tam/adapters/adium_spec.rb
@@ -12,19 +12,19 @@ describe TamTam::Adapters::Adium do
       matches = Dir["#{fixtures}/GTalk*/**/*.xml"]
       messages = subject.load_messages(matches)
       expect(messages).to respond_to(:each)
-      expect(messages.first.keys).to eq([:sender, :text, :time])
+      expect(messages.first.keys).to match_array([:sender, :text, :time])
     end
   end
 
   describe "#as" do
     it "filters the logs by a single account" do
-      expect(subject.as(["bongo"])).to eq(
+      expect(subject.as(["bongo"])).to match_array(
         Dir["#{fixtures}/AIM.bongo/**/*.xml"]
       )
     end
 
     it "filters the logs by multiple accounts" do
-      expect(subject.as(["bongo", "bongo@gmail.com"])).to eq(
+      expect(subject.as(["bongo", "bongo@gmail.com"])).to match_array(
         Dir["#{fixtures}/**/*.xml"]
       )
     end
@@ -32,13 +32,13 @@ describe TamTam::Adapters::Adium do
 
   describe "#with" do
     it "filters the logs by a single participant" do
-      expect(subject.with(["bongita"])).to eq(
+      expect(subject.with(["bongita"])).to match_array(
         Dir["#{fixtures}/AIM.bongo/**/*.xml"]
       )
     end
 
     it "filters the logs by multiple participants" do
-      expect(subject.with(["bongita", "rumples@gmail.com"])).to eq(
+      expect(subject.with(["bongita", "rumples@gmail.com"])).to match_array(
         Dir["#{fixtures}/**/*.xml"]
       )
     end
@@ -46,7 +46,7 @@ describe TamTam::Adapters::Adium do
 
   describe "#on" do
     it "filters the logs by date" do
-      expect(subject.on(Time.new(2011, 7, 23))).to eq(
+      expect(subject.on(Time.new(2011, 7, 23))).to match_array(
         Dir["#{fixtures}/AIM.bongo/bongita/*07-23*/*.xml"]
       )
     end
@@ -57,7 +57,7 @@ describe TamTam::Adapters::Adium do
       start = Time.new(2011, 7, 23)
       stop = Time.new(2011, 7, 24)
 
-      expect(subject.between(start, stop)).to eq(
+      expect(subject.between(start, stop)).to match_array(
         Dir["#{fixtures}/AIM.bongo/**/*.xml"]
       )
     end
